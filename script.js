e1e6e46ac8216b5c20b6c6d99a8d19ceb881c1e6
EmiliaLaraS <emilia.lara.s@mail.pucv.cl>
2023-11-05 18:01:17 -0300

    avance proyecto (se agrego en el javascript carrito de compras y se arreglaron detalles)

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+/*-----------------------------------------------------------------------------*/
+
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
